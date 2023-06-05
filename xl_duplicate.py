2f8ce071f1225bc3e44554ca1dd49cff3d658206
cfaith907 <50335516+cfaith907@users.noreply.github.com>
2023-06-05 18:37:30 -0500

    Add files via upload

--- xl_duplicate.py
+++ xl_duplicate.py
@@ -1,25 +1,26 @@
 import xlrd
 # file location
-loc = ("c:\\Users\\connor.faith\\Desktop\\tests.xls")# open file
-xfile = xlrd.open_workbook(loc)# 0 is the index of sheet in file i.e. 1st sheet
-# We can read 3rd sheet if our file had multiple sheets like this: xfile.sheet_by_index(2)
+loc = ("c:\\Users\\connor.faith\\Desktop\\tests.xls")
+# open file
+xfile = xlrd.open_workbook(loc)
+#indexing 1st sheet with (0)
 sheet = xfile.sheet_by_index(0)
 
 # print numb of rows
 print("there are %i rows" % sheet.nrows) 
 
-# define empty array
+# defining empty arrays to pull dupes into 
 duplicates = []
 bin_pallet = [] 
 
-# loop through all rows
+# loop through all rows in sheet 1
 for i in range(sheet.nrows):
     # get value of current column sheet.cell_value(i, 0) or i, 1 etc
     value = sheet.cell_value(i, 0)
 
     # check if value already exits
     # if true, add to dupes array
-    # if false, at too notdupes **
+    # if false, at too notdupes array
     if value in bin_pallet:
         duplicates.append(sheet.cell_value(i, 0))
     else:
