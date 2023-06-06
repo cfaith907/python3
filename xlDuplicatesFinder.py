1d8ecc1f52fc7d38be5389906ee04d8233f6755b
cfaith907 <50335516+cfaith907@users.noreply.github.com>
2023-06-05 22:33:40 -0500

    Add files via upload

---
+++ xlDuplicatesFinder.py
@@ -0,0 +1,34 @@
+import xlrd
+import sys
+# file must be xls to run in current setup 
+text = input("Drop xls file here to find duplicates")
+loc = (text) # insert location of xls file here 
+# open file
+xfile = xlrd.open_workbook(loc)
+#indexing 1st sheet with (0)
+sheet = xfile.sheet_by_index(0)
+
+# print numb of rows
+print("there are %i rows" % sheet.nrows) 
+
+# defining empty arrays to pull dupes into 
+duplicates = []
+bin_pallet = [] 
+
+# loop through all rows in sheet 1
+for i in range(sheet.nrows):
+    # get value of current column sheet.cell_value(i, 0) or i, 1 etc
+    value = sheet.cell_value(i, 0)
+
+    # check if value already exits
+    # if true, add to dupes array
+    # if false, at too notdupes array
+    if value in bin_pallet:
+        duplicates.append(sheet.cell_value(i, 0))
+    else:
+        bin_pallet.append(sheet.cell_value(i, 0))
+        
+print("%i duplicate/s found" % len(duplicates))
+# return how many dupes identified 
+print(duplicates)
+# return duplicates themselves 
